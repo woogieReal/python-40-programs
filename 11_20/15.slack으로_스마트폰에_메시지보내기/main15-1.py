55434a322f1cce0b88975b0a27c324be682d0584
woogieReal <woogiereal@gmail.com>
2023-05-29 13:18:06 +0900

    15장 코드 수정

--- 11_20/15.slack으로_스마트폰에_메시지보내기/main15-1.py
+++ 11_20/15.slack으로_스마트폰에_메시지보내기/main15-1.py
@@ -1,22 +1,30 @@
+"""
+봇(로봇) 채널로 메시지 보내는 코드 만들기
+"""
+
 import requests
 import json
 
-slack_webhook_url = "https://hooks.slack.com/services/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"
+# 자신의 python_bot의 생성된 webhook url을 넣는다.
+slack_webhook_url = "https://hooks.slack.com/services/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"
 
+# webhook 방식으로 메시지를 보내는 함수
 def sendSlackWebhook(strText):
     headers = {
-    "Content-type": "application/json"
+        "Content-type": "application/json"
     }
 
     data = {
-        "text" : strText
+        "text": strText
     }
 
-    res = requests.post(slack_webhook_url, headers=headers, data=json.dumps(data))
-    
+    res = requests.post(slack_webhook_url, headers=headers,
+                        data=json.dumps(data))
+
     if res.status_code == 200:
         return "ok"
     else:
         return "error"
+
 
 print(sendSlackWebhook("안녕하세요 파이썬에서 보내는 메시지 입니다."))
