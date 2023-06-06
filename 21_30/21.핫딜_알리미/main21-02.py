90e7bb331bdc6ce02686baf20193812170659fb2
woogieReal <woogiereal@gmail.com>
2023-06-06 21:10:38 +0900

    21장 코드 추가

---
+++ 21_30/21.핫딜_알리미/main21-02.py
@@ -0,0 +1,35 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+import telegram
+import time
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+send_message_list = []
+
+while True:
+    try:
+        driver.get(url='https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
+        driver.implicitly_wait(time_to_wait=10)
+        titles = driver.find_elements(By.CSS_SELECTOR, '#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr > td:nth-child(2) > div > a > font')
+        urls = driver.find_elements(By.CSS_SELECTOR, '#revolution_main_table > tbody > tr > td:nth-child(3) > table > tbody > tr > td:nth-child(2) > div > a')
+        message = ""
+        for i in range(len(titles)):
+            if "김치" in titles[i].text:
+                message = titles[i].text + "\n" + urls[i].get_attribute('href')
+                if message not in send_message_list:
+                    print(message)
+                    send_message_list.append(message)
+                    token = os.environ.get('TELEGRAM_TOKEN')
+                    id = os.environ.get('TELEGRAM_CHAT_ID')
+                    bot = telegram.Bot(token)
+                    bot.send_message(chat_id=id, text=message)
+        
+        time.sleep(60.0*5)
+    except KeyboardInterrupt:
+        break
