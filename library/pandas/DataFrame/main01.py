dd371c1799767808075747f97961d1c06615cdc8
woogieReal <woogiereal@gmail.com>
2023-06-09 15:35:22 +0900

    pandas 라이브러리 연습 파일명 변경

--- library/pandas/DataFrame/main01.py
+++ library/pandas/DataFrame/main01.py
@@ -8,7 +8,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 df1 = pd.DataFrame([['a', 'b'], ['c', 'd']],
                    index=['row 1', 'row 2'],
                    columns=['col 1', 'col 2'])
-df1.to_excel("output1.xlsx")
+df1.to_excel("output01-01.xlsx")
 
 # 시트 이름 지정
 df1.to_excel("output2.xlsx",
@@ -16,11 +16,11 @@ df1.to_excel("output2.xlsx",
 
 # 하나 이상의 시트를 작성하기 위해서는 ExcelWriter 객체를 사용해야 한다.
 df2 = df1.copy()
-with pd.ExcelWriter('output3.xlsx') as writer:
+with pd.ExcelWriter('output01-03.xlsx') as writer:
     df1.to_excel(writer, sheet_name='Sheet_name_1')
     df2.to_excel(writer, sheet_name='Sheet_name_2')
 
 # 기존 엑셀 파일에 더할 때도 ExcelWriter 를 사용한다.
-with pd.ExcelWriter('output3.xlsx',
+with pd.ExcelWriter('output01-03.xlsx',
                     mode='a') as writer:
     df2.to_excel(writer, sheet_name='Sheet_name_3')
