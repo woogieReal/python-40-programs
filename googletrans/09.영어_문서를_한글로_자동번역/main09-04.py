401c1e882ae5cce6ec633100f950833f420f418d
woogieReal <woogiereal@gmail.com>
2023-09-28 20:30:19 +0900

    googletrans 라이브러리 디렉터리 분리

--- googletrans/09.영어_문서를_한글로_자동번역/main09-04.py
+++ googletrans/09.영어_문서를_한글로_자동번역/main09-04.py
@@ -1,20 +1,25 @@
 """
-영어 문서를 한글로 번역하는 코드 만들기
+번역 내용을 새 파일로 저장하는 코드 만들기
 """
 
 from os import linesep
 import googletrans
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 translator = googletrans.Translator()
 
 # 파일을 읽어올 경로를 지정
-read_file_path = "01_10/09.영어_문서를_한글로_자동번역/영어파일.txt"
+read_file_path = "영어파일.txt"
+write_file_path = "한글파일.txt"
 
 # 파일에서 줄별로 읽어 리스트 형태로 바인딩
 with open(read_file_path, 'r') as f:
     readLines = f.readlines()
 
-# 리스트 형태로 저장된 readLines에서 한 줄씩 한글로 변환하여 출력
 for lines in readLines:
     result1 = translator.translate(lines, dest='ko')
     print(result1.text)
+    with open(write_file_path, 'a', encoding='UTF8') as f:
+        f.write(result1.text + '\n')
