2278b4f6a85c92d60ba8016cc9bda348b5f45d6d
woogieReal <woogiereal@gmail.com>
2023-09-28 19:00:56 +0900

    flask 라이브러리 폴더 분리

--- flask/28.플라스크_웹서버_만들기/main28-03.py
+++ flask/28.플라스크_웹서버_만들기/main28-03.py
@@ -17,7 +17,7 @@ def map():
 
 def main():
     # flask 웹서버를 실행
-    app.run(debug=True, port=80)
+    app.run(debug=True, port=3000)
 
 # 코드를 직접 실행 시 main() 함수를 실행
 # __name__은 코드를 직접 실행 시 이름이 __main__이다
